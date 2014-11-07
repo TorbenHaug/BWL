0829e48582f00dc1674c0a5416b2168e8fc80825
MrTorgue <andreas.berks@haw-hamburg.de>
2014-11-08 00:54:17 +0100

    Added model for homepage news.

---
+++ Scripts/generate_models.bat
@@ -0,0 +1,11 @@
+rails generate scaffold User firstname:string lastname:string email:string password:string role:integer deleted_at:datetime --force
+rails generate scaffold Tag name:string --force
+rails generate scaffold Article name:string description:string price:float deleted_at:datetime --force
+rails generate scaffold ArticleStructure amount:integer deleted_at:datetime --force
+rails generate scaffold Menu name:string --force
+rails generate scaffold ShoppingCartEntry amount:integer --force
+rails generate scaffold Bill --force
+rails generate scaffold BillEntry amount:integer unit_price:float --force
+rails generate scaffold MenuToTag --force
+rails generate scaffold ArticleToTag --force
+rails generate scaffold News title:string content:string --force
--- WebShop/config/routes.rb
+++ WebShop/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :news
+
   resources :article_to_tags
 
   resources :menu_to_tags
