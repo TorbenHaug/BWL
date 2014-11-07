913771a995ca0d2d108b31d2c3f9b028f392c629
MrTorgue <andreas.berks@haw-hamburg.de>
2014-11-07 20:07:49 +0100

    Added relation between ShoppingCartEntry and User and Article.

--- WebShop/app/models/shopping_cart_entry.rb
+++ WebShop/app/models/shopping_cart_entry.rb
@@ -1,6 +1,10 @@
 class ShoppingCartEntry < ActiveRecord::Base
+  self.primary_keys = :user_id, :article_id
+  
   validates_presence_of :amount
   validates_numericality_of :amount, greater_than: 0
+  validates_presence_of :user_id
+  validates_presence_of :article_id
   
   belongs_to :user
   belongs_to :article
--- WebShop/db/migrate/20141107163922_create_shopping_cart_entries.rb
+++ WebShop/db/migrate/20141107163922_create_shopping_cart_entries.rb
@@ -1,9 +1,14 @@
 class CreateShoppingCartEntries < ActiveRecord::Migration
   def change
-    create_table :shopping_cart_entries do |t|
+    create_table :shopping_cart_entries, :id => false do |t|
       t.integer :amount
+      
+      t.integer :user_id
+      t.integer :article_id
 
       t.timestamps
     end
+    
+    execute "ALTER TABLE article_structures ADD PRIMARY KEY (user_id, article_id);"
   end
 end
