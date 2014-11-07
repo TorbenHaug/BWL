41188638b0be5a53960bf9b3026ba93de014e430
MrTorgue <andreas.berks@haw-hamburg.de>
2014-11-07 20:18:54 +0100

    Removed unnessary relation

--- WebShop/app/models/article.rb
+++ WebShop/app/models/article.rb
@@ -4,7 +4,6 @@ class Article < ActiveRecord::Base
   validates_numericality_of :price, greater_than_or_equal_to: 0
   
   has_many :tags
-  has_many :article_structures
   has_many :shopping_cart_entries
   
   has_many :article_structure_to, :foreign_key => 'upper_part_id', :class_name => 'ArticleStructure'
