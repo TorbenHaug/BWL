6172f48c667b95e0a4eac763247933672d625546
TorbenHaug <claustorbenhaug@hotmail.de>
2014-11-08 16:58:16 +0100

    added bcrypt

--- WebShop/app/models/user.rb
+++ WebShop/app/models/user.rb
@@ -8,7 +8,15 @@ class User < ActiveRecord::Base
   validates_presence_of :role
   validates_numericality_of :role
   validates_inclusion_of :role, in: 0..200
-  
+
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+
   has_many :shopping_cart_entries
   has_many :bill
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+        BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
