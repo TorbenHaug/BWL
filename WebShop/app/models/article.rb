103bde9bfdd5cee85801574949ad61ff7317d25e
MrTorgue <andreas.berks@haw-hamburg.de>
2014-11-08 14:13:17 +0100

    Restored validations and view of article.

--- WebShop/app/models/article.rb
+++ WebShop/app/models/article.rb
@@ -1,2 +1,11 @@
 class Article < ActiveRecord::Base
+  validates_presence_of :name
+  validates_presence_of :price
+  validates_numericality_of :price, greater_than_or_equal_to: 0
+  
+  has_many :shopping_cart_entries
+  has_many :article_to_tags
+  
+  has_many :article_structure_to, :foreign_key => 'upper_part_id', :class_name => 'ArticleStructure'
+  has_many :article_structure_from, :foreign_key => 'lower_part_id', :class_name => 'ArticleStructure'
 end
