a4af61036b31ac17ce9904cf8211683957aaceef
MrTorgue <andreas.berks@haw-hamburg.de>
2014-11-08 17:29:50 +0100

    Extended test article data.

--- WebShop/app/controllers/application_controller.rb
+++ WebShop/app/controllers/application_controller.rb
@@ -38,15 +38,25 @@ class ApplicationController < ActionController::Base
     # create tags and menus
     generate_menu_with_tag("Komplettpakete")
     generate_menu_with_tag("Desktop-PCs")
-    generate_menu_with_tag("Mainboard Bundles")
+    generate_menu_with_tag("Monitore")
+    generate_menu_with_tag("Mainboard-Bundles")
     generate_menu_with_tag("Mainboards")
     generate_menu_with_tag("CPUs")
+    generate_menu_with_tag("Arbeitsspeicher")
     
     # create articles
+    generate_article_with_tags("[SET] DELL PowerEdge T20 Xeon E3-1225v3 Mini-Tower Server with Samsung S22D300H 54,61 cm (22 Zoll) LED-Monitor",
+      "Der PowerEdge T20 Server überzeugt mit umfangreicher interner Massenspeicherkapazität und starker Leistung. Sein Minitower-Gehäuse ist für einen leisen, effizienten und unterbrechungsfreien Betrieb ausgelegt. Mit dem T20 können Sie die Daten und Anwendungen mehrerer Desktop-PCs und Workstations auf einem einzigen Server konsolidieren und so nicht nur die Zusammenarbeit optimieren, sondern auch Ihre Produktivität steigern.",
+      379.00,
+      ["Komplettpakete"])
     generate_article_with_tags("DELL PowerEdge T20 Xeon E3-1225v3 Mini-Tower Server",
       "Der PowerEdge T20 Server überzeugt mit umfangreicher interner Massenspeicherkapazität und starker Leistung. Sein Minitower-Gehäuse ist für einen leisen, effizienten und unterbrechungsfreien Betrieb ausgelegt. Mit dem T20 können Sie die Daten und Anwendungen mehrerer Desktop-PCs und Workstations auf einem einzigen Server konsolidieren und so nicht nur die Zusammenarbeit optimieren, sondern auch Ihre Produktivität steigern.",
       299.00,
       ["Desktop-PCs"])
+    generate_article_with_tags("Samsung S22D300H 54,61 cm (22 Zoll) LED-Monitor",
+      "Der neuen Samsung Monitore S22D300H überzeugen durch sein schlichtes, klassisches Design in schwarz-glänzender Klavierlack-Optik. Darüber hinaus verfügt er über einen HDMI- und VGA-Anschluss, sowie praktische Features: MagicUpscale ermöglicht durch einen intelligenten Algorithmus qualitativ hochwertiges Upscaling von Bildinhalten. Ist die Funktion aktiviert, sehen auch Bildinhalte in geringerer Auflösung nahezu so perfekt aus als würden sie in nativer Full-HD-Qualität zur Verfügung stehen. Die Eco Saving-Funktion ermöglicht Energiesparen auf Knopfdruck durch Anpassung der Bildhelligkeit. Selbstredend sind alle Modelle Windows 8 kompatibel.",
+      99.00,
+      ["Monitore"])
     generate_article_with_tags("Acer Aspire XC-605 DT.STEEG.084 PC i7-4790 8GB 1TB SSHD",
       "Aufgaben erledigen, Programme ansehen, gelegentlich ein Spiel spielen und mit Freunden in Kontakt bleiben: All dies lässt sich ganz einfach mit einem Intel® Core™-Prozessor bis zur dritten Generation oder einem neuen AMD-Prozessor und Grafikprozessoren bewerkstelligen, die von integrierten Intel® HD-Grafikkarten bis hin zu diskreten NVIDIA® GeForce®- oder AMD Radeon™-Karten reichen.",
       499.00,
@@ -55,16 +65,49 @@ class ApplicationController < ActionController::Base
       "Der platzsparende und zuverlässige Lenovo H500s verbindet neueste Prozessortechnologie mit erstklassiger Multimedialeistung und großer Speicherkapazität – und ist so ideal für Heimcomputer-Anwendungen.",
       159.00,
       ["Desktop-PCs"])
+    generate_article_with_tags("Asus P9D WS Intel® C226",
+      "Das Asus P9D WS-Mainboard ist mit dem neuen Intel C226-Chipsatz ausgestattet und unterstützt Intel Xeon E3-1200 v3- sowie Core-Prozessoren der vierten Generation. Das Asus P9D WS ist voll ISV-zertifiziert (Independent Software Vendor) und wurde speziell für Workstations sowie High-End-Desktop-PC-Systeme entwickelt und verfügt über eine breite Speicherkompatibilität. Es ist mit dem Asus Dr. Power-Dienstprogramm ausgestattet, welches den Status von Netzteilen überwacht, um plötzlichen Systemausfällen vorzubeugen. Außerdem verfügt es über eine Vielzahl an Schnittstellen inklusive mehrerer Schnittstellen für Monitore.",
+      0.00,
+      ["Mainboards"])
+    generate_article_with_tags("Intel Xeon E3-1225 v3",
+      "Die Intel Xeon Prozessoren zeichnen sich durch neue, innovative Technologien wie die Hyper-Threading-Technologie und die Intel NetBurst Mikroarchitektur aus. Sie bieten hervorragende Leistungs- und Funktionseigenschaften mit genügend Spielraum für aktuelle und kommende Server- und Workstation-Plattformen. Bedeutende Weiterentwicklungen der Plattform, die besonderen Leistungseigenschaften und ein außergewöhnliches Preis-Leistungs-Verhältnis machen es gerade jetzt sinnvoll, von älteren Systemen auf die aktuelle Technologie aufzurüsten und damit Vorteile im Wettbewerb zu erzielen.",
+      0.00,
+      ["CPUs"])
+    generate_article_with_tags("Crucial CT2KIT51264BA160B 8GB 4GB 240-pin DDR3 1600mhz non-ECC",
+      "Crucial 8GB KIT (4GBX2) DDR3 1600 MT/S, 4GB, 240-pin DIMM, DDR3 PC3-12800, 1600MHz, Non-ECC, CL11, 1.5V ",
+      0.00,
+      ["Arbeitsspeicher"])
+    generate_article_with_tags("[BUNDLE] Asus P9D WS Intel® C226 with Intel Xeon E3-1225 v3",
+      "Mainboard / CPU Bundles sind ideal als Grundgerüst für ein individuell gestaltetes PC-System geeignet. Darüber hinaus stellen Sie eine erstklassige Möglichkeit dar, um einem bestehenden PC-System ein Upgrade mit neuer, leistungsstarker Technik zu verschaffen. Unsere Mainboard / CPU Bundles werden mit den Komponenten CPU, passendem Kühler und Mainboard sowie dem notwendigen Zubehör wie Schrauben, Kabel und Bedienungsanleitung ausgeliefert!",
+      0.00,
+      ["Mainboard-Bundles"])
     
     # create structure
+    
     generate_article_structure(
-      "Lenovo IdeaCentre H500s 57324437 PC J1750 4GB 500GB",
-      "Acer Aspire XC-605 DT.STEEG.084 PC i7-4790 8GB 1TB SSHD",
-      3)
-    generate_article_structure(
-      "Acer Aspire XC-605 DT.STEEG.084 PC i7-4790 8GB 1TB SSHD",
+      "[SET] DELL PowerEdge T20 Xeon E3-1225v3 Mini-Tower Server with Samsung S22D300H 54,61 cm (22 Zoll) LED-Monitor",
       "DELL PowerEdge T20 Xeon E3-1225v3 Mini-Tower Server",
-      3)
+      1)
+    generate_article_structure(
+      "[SET] DELL PowerEdge T20 Xeon E3-1225v3 Mini-Tower Server with Samsung S22D300H 54,61 cm (22 Zoll) LED-Monitor",
+      "Samsung S22D300H 54,61 cm (22 Zoll) LED-Monitor",
+      1)
+    generate_article_structure(
+      "DELL PowerEdge T20 Xeon E3-1225v3 Mini-Tower Server",
+      "[BUNDLE] Asus P9D WS Intel® C226 with Intel Xeon E3-1225 v3",
+      1)
+    generate_article_structure(
+      "[BUNDLE] Asus P9D WS Intel® C226 with Intel Xeon E3-1225 v3",
+      "Intel Xeon E3-1225 v3",
+      1)
+    generate_article_structure(
+      "[BUNDLE] Asus P9D WS Intel® C226 with Intel Xeon E3-1225 v3",
+      "Asus P9D WS Intel® C226",
+      1)
+    generate_article_structure(
+      "[BUNDLE] Asus P9D WS Intel® C226 with Intel Xeon E3-1225 v3",
+      "Crucial CT2KIT51264BA160B 8GB 4GB 240-pin DDR3 1600mhz non-ECC",
+      2)
 
     render text: "Generated database entries!"
   end
