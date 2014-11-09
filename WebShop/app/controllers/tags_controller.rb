fd678f56db0c7cf5ee40da0eb21412e7380844e1
TorbenHaug <claustorbenhaug@hotmail.de>
2014-11-09 18:40:02 +0100

    added Tags to Admin menu and set security options

--- WebShop/app/controllers/tags_controller.rb
+++ WebShop/app/controllers/tags_controller.rb
@@ -4,35 +4,43 @@ class TagsController < ApplicationController
   # GET /tags
   # GET /tags.json
   def index
-    @tags = Tag.all
+    if check_rights(admin_user?)
+      @tags = Tag.all
+    end
   end
 
   # GET /tags/1
   # GET /tags/1.json
   def show
+    check_rights(admin_user?)
   end
 
   # GET /tags/new
   def new
-    @tag = Tag.new
+    if check_rights(admin_user?)
+      @tag = Tag.new
+    end
   end
 
   # GET /tags/1/edit
   def edit
+    check_rights(admin_user?)
   end
 
   # POST /tags
   # POST /tags.json
   def create
-    @tag = Tag.new(tag_params)
+    if check_rights(admin_user?)
+      @tag = Tag.new(tag_params)
 
-    respond_to do |format|
-      if @tag.save
-        format.html { redirect_to @tag, notice: 'Tag was successfully created.' }
-        format.json { render :show, status: :created, location: @tag }
-      else
-        format.html { render :new }
-        format.json { render json: @tag.errors, status: :unprocessable_entity }
+      respond_to do |format|
+        if @tag.save
+          format.html { redirect_to @tag, notice: 'Tag was successfully created.' }
+          format.json { render :show, status: :created, location: @tag }
+        else
+          format.html { render :new }
+          format.json { render json: @tag.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
@@ -40,13 +48,15 @@ class TagsController < ApplicationController
   # PATCH/PUT /tags/1
   # PATCH/PUT /tags/1.json
   def update
-    respond_to do |format|
-      if @tag.update(tag_params)
-        format.html { redirect_to @tag, notice: 'Tag was successfully updated.' }
-        format.json { render :show, status: :ok, location: @tag }
-      else
-        format.html { render :edit }
-        format.json { render json: @tag.errors, status: :unprocessable_entity }
+    if check_rights(admin_user?)
+      respond_to do |format|
+        if @tag.update(tag_params)
+          format.html { redirect_to @tag, notice: 'Tag was successfully updated.' }
+          format.json { render :show, status: :ok, location: @tag }
+        else
+          format.html { render :edit }
+          format.json { render json: @tag.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
@@ -54,10 +64,12 @@ class TagsController < ApplicationController
   # DELETE /tags/1
   # DELETE /tags/1.json
   def destroy
-    @tag.destroy
-    respond_to do |format|
-      format.html { redirect_to tags_url, notice: 'Tag was successfully destroyed.' }
-      format.json { head :no_content }
+    if check_rights(admin_user?)
+      @tag.destroy
+      respond_to do |format|
+        format.html { redirect_to tags_url, notice: 'Tag was successfully destroyed.' }
+        format.json { head :no_content }
+      end
     end
   end
 
