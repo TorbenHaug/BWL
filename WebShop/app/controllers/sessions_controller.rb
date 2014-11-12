fd9528fb75d908d68677c81a35f0e6ea763023b8
MrTorgue <andreas.berks@haw-hamburg.de>
2014-11-12 06:55:55 +0100

    Added redirect to previous page at user login/logout.

--- WebShop/app/controllers/sessions_controller.rb
+++ WebShop/app/controllers/sessions_controller.rb
@@ -1,20 +1,22 @@
 class SessionsController < ApplicationController
   def new
   end
+  
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       flash[:notice] = "Wilkommen " + user.firstname + " " + user.lastname
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to current_user
+      redirect_to ((session[:return_to].nil?) ? root_path : session[:return_to])
     else
       flash[:notice] = "Emailadresse oder Passwort ist falsch!"
       render 'new'
     end
   end
+  
   def destroy
     log_out if logged_in?
-    redirect_to root_path
+    redirect_to ((session[:return_to].nil?) ? root_path : session[:return_to])
   end
 end
