0bdf98033921039f2707fe7253f02ce9ead37126
MrTorgue <andreas.berks@haw-hamburg.de>
2014-11-14 01:22:40 +0100

    Redesign of login view and error messages.

--- WebShop/app/controllers/sessions_controller.rb
+++ WebShop/app/controllers/sessions_controller.rb
@@ -5,12 +5,11 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      flash[:notice] = "Wilkommen " + user.firstname + " " + user.lastname
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to ((session[:return_to].nil?) ? root_path : session[:return_to])
     else
-      flash[:notice] = "Emailadresse oder Passwort ist falsch!"
+      flash[:notice] = "Bei der Eingabe Ihrer E-Mail-Adresse/Ihres Kennworts ist ein Fehler aufgetreten.<br/>Bitte versuchen Sie es erneut.".html_safe
       render 'new'
     end
   end
