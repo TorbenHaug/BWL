a313d80fef8f6b90cc84e78b1b18576351a5320e
MrTorgue <andreas.berks@haw-hamburg.de>
2014-11-15 15:17:31 +0100

    Performance optimization.

--- WebShop/app/helpers/bills_helper.rb
+++ WebShop/app/helpers/bills_helper.rb
@@ -1,2 +1,18 @@
 module BillsHelper
+  def get_bills(articles)
+    article_bills = articles.map{|article|
+      BillEntry.where(:article => article).map{|entry| entry.bill}.uniq}
+    return article_bills.reduce(:&)
+  end
+  
+  def get_bill_count(articles)
+    bills = get_bills(articles)
+    return (bills.nil?) ? (0) : (bills.count)
+  end
+  
+  def get_buyed_amount(article)
+    BillEntry.where(:article => article).reduce(0){|accu, entry|
+      accu + entry.amount
+    }
+  end
 end
--- WebShop/app/helpers/menus_helper.rb
+++ WebShop/app/helpers/menus_helper.rb
@@ -1,2 +1,31 @@
 module MenusHelper
+  def get_menus_of_article(article)
+    result = []
+    ArticleToTag.where(:article => article).each{|article_item|
+      MenuToTag.where(:tag => article_item.tag).each{|menu_item|
+        result.push(menu_item.menu) if (!result.include?(menu_item.menu))
+      }
+    }
+    return result
+  end
+  
+  def get_articles_of_menu(menu)
+    result = []
+    MenuToTag.where(:menu => menu).each{|menu_item|
+      ArticleToTag.where(:tag => menu_item.tag).each{|article_item|
+        result.push(article_item.article) if (!result.include?(article_item.article))
+      }
+    }
+    return result
+  end
+  
+  def get_place_at_menu(article, menu)
+    buyed_amounts = get_articles_of_menu(menu).map{|entry|
+      [entry, get_buyed_amount(entry)]}
+    buyed_amounts.sort!{|a, b| b[1] <=> a[1]}
+    
+    buyed_amounts.each_with_index{|item, index|
+      (return [index + 1, buyed_amounts.size]) if (item[0] == article)
+    }
+  end
 end
